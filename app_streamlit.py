2acb8a04eb8842c6cfd5d47724ec1d634de42d57
Michael Condon <michael.condon@interworks.nl>
2023-12-10 09:48:35 +0100

    added difficulty

--- app_streamlit.py
+++ app_streamlit.py
@@ -2,19 +2,20 @@ import random
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
+import  numpy as np
 
 
 def list_letters(phrase):
     list_of_letters = [i for i in phrase]
     return list_of_letters
 
-def generate_random_numbers(letter):
+def generate_random_numbers(letter, span):
     
     if letter.isalpha()==False:
         if letter == ' ':
             letter_value = 0
             # Generate 8 random numbers between -10 and 10
-            random_numbers = [random.randint(-15, 15) for _ in range(8)]
+            random_numbers = [random.randint(span[0], span[1]) for _ in range(8)]
             
             # Adjust the last number to ensure the sum is equal to 26 - the letter value
             random_numbers[-1] += (26 - letter_value - sum(random_numbers))
@@ -28,7 +29,7 @@ def generate_random_numbers(letter):
         letter_value = ord(letter.lower()) - ord('a') + 1
         
         # Generate 8 random numbers between -10 and 10
-        random_numbers = [random.randint(-15, 15) for _ in range(8)]
+        random_numbers = [random.randint(span[0], span[1]) for _ in range(8)]
         
         # Adjust the last number to ensure the sum is equal to 26 - the letter value
         random_numbers[-1] += (26 - letter_value - sum(random_numbers))
@@ -37,46 +38,94 @@ def generate_random_numbers(letter):
 
 
 
-# Example usage:
-#x_value = 5
-#result = find_numbers_for_sum(x_value)
-#print(f"Numbers for sum 27 - {x_value}: {result}")
-
-
 
 def convert_df(df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df.to_csv().encode('utf-8')
 
-st.title("Edgzzle")
-
-phrase = st.text_input("Enter a phrase:")
-
-if st.button("Generate Edgzzle"):
-
-    list_of_letters = list_letters(phrase)
-    cols = [i for i in range(len(list_of_letters))]
+def hint_row(list_of_letters, level):
     blanks = []
     for x in list_of_letters:
         if x.isalpha()==False:
             blanks.append(x)
         else:
             blanks.append(' ')
-    black_row = dict(zip(cols,blanks))
+   
+    cols = [i for i in range(len(list_of_letters))]
+   
+    if level == 'easy':
+        hint_length = int(np.round(len(list_of_letters)/2))
+        list_index = random.sample(range(0, len(list_of_letters)), hint_length)
+        for i,x in enumerate(list_index):
+            blanks[x] = list_of_letters[list_index[i]]    
+        
+        hint = dict(zip(cols,blanks))
+        return hint
+    
+    elif level == 'medium':
+        hint_length = int(np.round(len(list_of_letters)/4))
+        list_index = random.sample(range(0, len(list_of_letters)), hint_length)
+        for i,x in enumerate(list_index):
+            blanks[x] = list_of_letters[list_index[i]]
+        
+        hint = dict(zip(cols,blanks))
+        return hint
+    
+    else:
+        hint = dict(zip(cols,blanks))
+        return hint
+
+def trigger():
+    if 'df' not in st.session_state:
+        st.session_state['df'] = True
+
+st.title("Edgzzle")
+st.write("1. Enter a phrase and select the difficulty level to generate a puzzle")
+st.write("2. Sum the numbers in each column to get the pass number")
+st.write("3. Subtract the pass number from 26: this is the number of the letter in the alphabet")
+
+phrase = st.text_input("Enter a phrase:")
+
+col1, col2, col3 = st.columns(3)
+
+
+
+with col1:
+    if st.button("Easy Edgzzle"):
+        trigger()
+        span = [-3, 3]
+        list_of_letters = list_letters(phrase)
+        hint_row = hint_row(list_of_letters, 'easy')
+        
+
+with col2:
+    if st.button("Medium Edgzzle"):
+        trigger()
+        span = [-6, 6]
+        list_of_letters = list_letters(phrase)
+        hint_row = hint_row(list_of_letters, 'medium')
+ 
+        
+
+with col3:
+    if st.button("Hard Edgzzle"):
+        trigger()
+        span = [-10, 10]
+        list_of_letters = list_letters(phrase)
+        hint_row = hint_row(list_of_letters, 'hard')
+                
+if 'df' in st.session_state:
     df = pd.DataFrame()
     for i, x in enumerate(list_of_letters):
-        df[i] = generate_random_numbers(x)
-        
-    
-    df = pd.concat([df.iloc[:4], pd.DataFrame([black_row]), df.iloc[4:]]).reset_index(drop=True)
-    st.dataframe(df, hide_index=True)
+        df[i] = generate_random_numbers(x, span)
+    df = pd.concat([df.iloc[:4], pd.DataFrame([hint_row]), df.iloc[4:]]).reset_index(drop=True)
+    st.dataframe(df, hide_index=True, use_container_width=True)
     # Create a Plotly table
     fig = go.Figure(data=[go.Table(
         header=dict(values=df.columns),
         cells=dict(values=df.values.T)
     )])
 
- 
     csv = convert_df(df)
 
     st.download_button(
@@ -85,3 +134,4 @@ if st.button("Generate Edgzzle"):
         file_name='Edgzzle.csv',
         mime='text/csv',
     )
+
